2ac82f7d32e0e1a699f28c6d7fdb1b3165ebe128
Maha <mahaucsb@gmail.com>
2014-03-18 11:26:21 -0700

    run.sh added

--- build/run.sh
+++ build/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
-  echo "Usage: `basename $0` <sign> <numDocuments> <cluedata=1,tweets=2,emails=3,ymusic=4,gnews=5,wiki=6>"
-  exit 3
+  echo "Usage: `basename $0` <sign> <numDocuments> <cluedata=1,tweets=2,emails=3,ymusic=4,gnews=5,wiki=6> <partition? Y/N"
+  exit 4
 fi
 
-bash hybrid/run.sh $1 $2 $3
+bash hybrid/run.sh $1 $2 $3 $4
 
 ############################################################
 # Environment Variables Set
